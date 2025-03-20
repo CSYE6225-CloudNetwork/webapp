d14ab9eaa6551c1a6ce96db5d5537e3237877866
AryaaPrashantHanamar <hanamar.a@northeastern.edu>
2025-03-20 00:49:39 -0400

    addind deafault creds

--- webapp/src/main/java/com/CSYE6225/webapp/Config/S3Config.java
+++ webapp/src/main/java/com/CSYE6225/webapp/Config/S3Config.java
@@ -1,5 +1,6 @@
 package com.CSYE6225.webapp.Config;
 
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
@@ -9,7 +10,7 @@ public class S3Config {
    public static S3Client getS3Client(String region){
       return S3Client.builder()
                .region(Region.of(region))
-               .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
+               .credentialsProvider(DefaultCredentialsProvider.create())
                .build();
    }
 
