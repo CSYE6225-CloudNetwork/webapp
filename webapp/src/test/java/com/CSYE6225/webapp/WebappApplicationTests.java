b8d305fafa2ad80e654630a7fb666c19d0fddeda
AryaaPrashantHanamar <hanamar.a@northeastern.edu>
2025-03-20 00:00:38 -0400

    revoke tests

---
+++ webapp/src/test/java/com/CSYE6225/webapp/WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CSYE6225.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
